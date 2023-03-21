19414604c9b6f148a1e9eb6805ade7fc9edf705a
rimasrom <hembromsamir715@gmail.com>
2023-03-21 18:26:30 +0530

    static database created

--- my-booklist/src/App.js
+++ my-booklist/src/App.js
@@ -1,6 +1,41 @@
 import React from 'react'
 import './App.css'
 
+const bookDatabase = {
+  javascript : [
+    {
+      name:"Eloquent JavaScript",
+      rating:"4/5"
+    },
+    {
+      name:"You Don't Know JS",
+      rating:"3.5/5"
+    }
+  ],
+
+  fiction: [
+    {
+      name: "Shiva Trilogy",
+      rating: "5/5"
+    },
+    {
+      name: "Harry Potter and the Sorcerer's Stone",
+      rating: "4.5/5"
+    }
+  ],
+
+  business: [
+    {
+      name: "Never Split the Difference",
+      rating: "3.5/5"
+    },
+    {
+      name: "Loonshots",
+      rating: "5/5"
+    }
+  ]
+}
+
 const App = () => {
   return (
     <div className='container-main'>
