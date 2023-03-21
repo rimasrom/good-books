c5e3a5aa9d37ba7c61ca00dcc1b39dec033ff0e1
rimasrom <hembromsamir715@gmail.com>
2023-03-21 17:57:47 +0530

    button created

--- my-booklist/src/App.js
+++ my-booklist/src/App.js
@@ -5,6 +5,11 @@ const App = () => {
     <div>
       <h1>📚 Good Books</h1>
       <p>Checkout my favorite books. Select a genre to get started</p>
+      <div className="button">
+        <button>JavaScript</button>
+        <button>Fiction</button>
+        <button>Business</button>
+      </div>
     </div>
   )
 }
